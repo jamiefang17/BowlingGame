80077ee29e629dcb3baddbc39b05c9e2cf4473fc
jamiefang17 <jamiefang17@gmail.com>
2023-11-15 21:44:40 -0800

    allOnesGameShouldScoreTwenty complete

--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -1,44 +1,29 @@
-// tests
-
 import static org.junit.Assert.assertEquals;
+
 
 import org.junit.Test;
 
+// tests
 public class BowlingGameTest {
-    // @Test
-    public void allGuttersGameScoreShouldBeZero() {
-        Game game = new Game();
-        for (int index = 0; index < 20; index++) {
-            game.roll(0);
+    private static void rollMany(Game game, int rolls,int pins){
+        for(int roll = 0; roll < rolls; roll++){
+            game.roll(pins);
         }
-        assertEquals(0, game.score());
     }
 
-
-
-    /**
-     * 
-     */
-    // @Test
-    public void allOnesGameShouldScoreTwenty() {
-        Game game = new Game();
-        for (int index = 0; index < 20; index++) {
-            game.roll(1);
-        }
-        assertEquals(20, game.score());
-    }
-
-    @Test
-    public void spareShouldGetNextRollBonus() {
-        Game game = new Game();
-        game.roll(5);
-        game.roll(5);
-        game.roll(3);
-        rollMany(game, 17, 0);
-        assertEquals(16, game.score());
-    }
-
-    // other code ...
-    private void rollMany(Game game, int i, int j) {
-    }
+    
+@Test
+public void allGuttersGameScoreShouldBeZero() {
+Game game = new Game();
+rollMany(game, 20, 0);
+assertEquals(0, game.score());
+}
+@Test
+public void allOnesGameShouldScoreTwenty() {
+Game game = new Game();
+rollMany(game, 20, 1);
+assertEquals(20, game.score());
+}
+// other tests ...
+// helper methods ...
 }
--- src/Game.java
+++ src/Game.java
@@ -1,27 +1,10 @@
 // code
-public class Game
-{
-// other code ...
-public int score(int[] _rolls) {
-int score = 0;
-int roll = 0;
-for (int frame = 0; frame < 10; frame++) {
-if (isSpareFrame(roll)) {
-score += 10 + _rolls[roll + 2];
-} else {
-score += _rolls[roll] + _rolls[roll + 1];
+public class Game {
+private Integer _score = 0;
+public void roll(int pins) {
+_score += pins;
 }
-roll += 2;
+public int score() {
+return _score;
 }
-return score;
-}
-private boolean isSpareFrame(int roll) {
-    return false;
-}
-public void roll(int i) {
-}
-public Object score() {
-	return null;
-}
-
 }
